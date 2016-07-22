285f2658068533cd6671c540e4a96da64ffab939
byronpolley <b_b0t@Byrons-MacBook-Pro-2.local>
2016-07-22 15:59:47 +0200

    About page addition

--- js/main.js
+++ js/main.js
@@ -1,52 +1,7 @@
-jQuery(function($){
+$(document).ready(function() {
 
-var bamyan = window.bamyan || {};
-
-/* ==================================================
-   Mobile Navigation
-================================================== */
-var mobileMenuClone = $('#menu').clone().attr('id', 'navigation-mobile');
-
-bamyan.mobileNav = function(){
-    var windowWidth = $(window).width();
-
-    if( windowWidth <= 979 ) {
-        if( $('#mobile-nav').length > 0 ) {
-            mobileMenuClone.insertAfter('#menu');
-            $('#navigation-mobile #menu-nav').attr('id', 'menu-nav-mobile');
-        }
-    } else {
-        $('#navigation-mobile').css('display', 'none');
-        if ($('#mobile-nav').hasClass('open')) {
-            $('#mobile-nav').removeClass('open');
-        }
-    }
+function myFunction() {
+    document.getElementById("logo").fill.color = "#fff";
 }
-
-bamyan.listenerMenu = function(){
-    $('#mobile-nav').on('click', function(e){
-        $(this).toggleClass('open');
-
-        if ($('#mobile-nav').hasClass('open')) {
-            $('#navigation-mobile').slideDown(500, 'easeOutExpo');
-        } else {
-            $('#navigation-mobile').slideUp(500, 'easeOutExpo');
-        }
-        e.preventDefault();
-    });
-
-    $('#menu-nav-mobile a').on('click', function(){
-        $('#mobile-nav').removeClass('open');
-        $('#navigation-mobile').slideUp(350, 'easeOutExpo');
-    });
-}
-
-
-    bamyan.nav();
-    bamyan.mobileNav();
-
-$(window).resize(function(){
-    bamyan.mobileNav();
-});
-
+    
 });
